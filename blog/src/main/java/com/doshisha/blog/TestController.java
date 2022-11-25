39b9816e051f93819a953d635cb4eb6122134be1
KimSeungJin <dahyun101107@naver.com>
2022-11-25 17:51:36 +0900

    API 적용 진행 중

--- blog/src/main/java/com/doshisha/blog/TestController.java
+++ blog/src/main/java/com/doshisha/blog/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
 
     @GetMapping("/")
     public String home(Model model){
-        return "home";
+        return "redirect:/login";
     }
     @GetMapping("/user")
     public String dispUser(Model model){
